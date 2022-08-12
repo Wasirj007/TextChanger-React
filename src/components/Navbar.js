94c40fc469b436322f23119c38570b5a91dd4220
wasiullah <wasirj720@gmail.com>
2022-08-12 19:59:59 +0500

    Project Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
     <>
       <div>
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-          <a className={`navbar-brand ml-2 text-${props.mode === 'light' ? 'dark' : 'light'}`} href="/" >{props.title}</a>
+          <a className={`navbar-brand ml-2 text-${props.mode === 'light' ? 'dark' : 'light'}`} href="" >{props.title}</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -14,11 +14,11 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse " id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className={`nav-link text-${props.mode === 'light' ? 'dark' : 'light'}`} href="/home">Home<span className="sr-only">(current)</span></a>
+                <a className={`nav-link text-${props.mode === 'light' ? 'dark' : 'light'}`} href="">Home<span className="sr-only">(current)</span></a>
               </li>
-              <li className="nav-item">
+              {/* <li className="nav-item">
                 <a className={`nav-link text-${props.mode === 'light' ? 'dark' : 'light'}`} href="/about">{props.AboutText}</a>
-              </li>
+              </li> */}
             </ul>
             <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
               <input className="form-check-input" onClick={props.RedMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
